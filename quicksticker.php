e8a74ad9b1a7b48e7869ef32898a69de8fb00919
Arron Lorenz <arronlorenz@gmail.com>
2011-02-02 03:20:03 -0600

    Second Commit

---
+++ quicksticker.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>quick sticker landing page</title>
+<link rel="stylesheet" type="text/css" href="./quicksticker.css" media="all" />
+<!--[if IE]>
+<style type="text/css" media="all">.borderitem {border-style:solid;}</style>
+<![endif]-->
+</head>
+
+<body>
+
+<div id="main">
+	<img src="images/quicksticker_r1_c2.gif" id="quicksticker_r1_c2"  alt="7 Years Durable" />
+		<img src="images/quicksticker_r1_c3.gif" id="quicksticker_r1_c3"  alt="" />
+		<img src="images/quicksticker_r1_c5.gif" id="quicksticker_r1_c5"  alt="100% Free Shipping" />
+		<br class="clearfloat" />
+	<img src="images/quicksticker_r2_c2.gif" id="quicksticker_r2_c2"  alt="" />
+		<br class="clearfloat" />
+	<img src="images/quicksticker_r3_c2.gif" id="quicksticker_r3_c2"  alt="seperator line" />
+		<br class="clearfloat" />
+	<p class="Txt_DOTLettering">
+
+		DOTLettering.com prides it self on offering the longest lasting vinyl we can at an affordable price! Owner/Operators and Large companies have relied on DOTLettering.com for more than lettering, our customers have come to expect the best quality, longest lasting, widest variety, and ease of application. Each and everyone of our orders is shipped for free. All of our custom products have a 90 day money back guarantee. If you&rsquo;re not satisfied with your order ship at back and we&rsquo;ll refund<br />%100 of your order the same day. Please don&rsquo;t hesitate to contact us for anything.<br /><br />Remember at DOTLettering.com our motto is:<br />&ldquo;US DOT Stickers should be Safe, Easy, and Legal!&rdquo;
+
+	</p>
+	<br class="clearfloat" />
+	<img src="images/quicksticker_r5_c2.gif" id="quicksticker_r5_c2"  alt="seperator line" />
+		<br class="clearfloat" />
+	<div id="colwrap1">
+		<img src="images/quicksticker_r6_c2.gif" id="quicksticker_r6_c2"  alt="7-10 Year Outdoor Durable Vinyl, Sticker Applies in one piece, Pick your size, choose and color or font, Money back guarantee" />
+				<img src="images/quicksticker_r7_c2.gif" id="quicksticker_r7_c2"  alt="USDOT Vinyl Stickers $7.99 per set" />
+			</div>
+	<img src="images/quicksticker_r6_c4.gif" id="quicksticker_r6_c4"  alt="USDOT sticker examples, and color examples" />
+		<br class="clearfloat" />
+</div>
+
+
+</body>
+</html>
